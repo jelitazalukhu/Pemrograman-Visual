8811af7da5240532d96f1de2514c4bee98595bcf
jelitazalukhu <jelitazalukhu463@gmail.com>
2026-09-08 11:34:24 +0700

    Materi Pertemuan 4

---
+++ Pertemuan4-perulangan/Pertemuan4-perulangan/FrmPerulangan.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Windows
+
+Public Class FrmPerulangan
+    Private Sub txtNilaiAwal_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilaiAwal.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtNilaiAkhirl_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilaiAkhir.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilaiAwal As Integer
+        Dim nilaiAkhir As Integer
+
+        If Not Integer.TryParse(txtNilaiAwal.Text, nilaiAwal) Then
+            MsgBox("Masukkan Dalam Bentuk Angka")
+            txtNilaiAwal.Focus()
+            Return
+        End If
+
+        If Not Integer.TryParse(txtNilaiAkhir.Text, nilaiAkhir) Then
+            MsgBox("Masukkan Dalam Bentuk Angka")
+            txtNilaiAkhir.Focus()
+            Return
+        End If
+
+        lstHasil.Items.Clear()
+
+        For i As Integer = nilaiAwal To nilaiAkhir Step 2
+            lstHasil.Items.Add(i)
+        Next
+    End Sub
+End Class
